00ebc0e837cc0ee503428412aa7aa1e5c09f52ea
mingyangShang <shangmingyanggo@gmail.com>
2017-05-20 11:00:23 +0800

    books list ui

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    most_popular_books = [Book("Book1", "https://img3.doubanio.com/lpic/s29436066.jpg", "isbn1", "author1", "1")]
+    most_popular_books = [Book("Book1", "https://img3.doubanio.com/lpic/s29436066.jpg", "isbn1", "author1", "1")] * 10
     return render_template('homepage.html', books=most_popular_books)
 
 @app.route('/books/<bookname>/')
-def book_info():
-    return render_template('homepage.html', books=[])
+def book_info(bookname):
+    return render_template('book.html', book=Book("Book1", "https://img3.doubanio.com/lpic/s29436066.jpg", "isbn1", "author1", "1"))
 
 @app.errorhandler(404)
 def page_not_found(error):
