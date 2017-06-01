00867dc2c307e4689ce8576c93750fa5dc119223
mingyangShang <shang_ming_yang@163.com>
2017-06-01 22:52:53 +0800

    add page up and down

--- static/js/userop.js
+++ static/js/userop.js
@@ -2,20 +2,46 @@
  * Created by shangmingyang on 5/20/17.
  */
 $(document).ready(function(){
-   $("#buy").click(buy)
+   $("#buy").click(buy);
+    $("#pre_page").click(prepage);
+    $("#next_page").click(nextpage);
+
 });
 
 function buy(){
-    // if($('#buy').text() == 'Buy'){
-    //     $("#buy").click(function(){
-    //         alert('Buy succeed!')
-    //         $("#buy").text('Buyed');
-    //     })
-    // }else{
-    //     $("#buy").
-    // }
     if($('#buy').text() == 'Buy'){
         alert('Buy succeed!')
         $("#buy").text('Buyed')
     }
+}
+
+function prepage(){
+    var currUrl = new Url(location.href);
+    var p = currUrl.query.page;
+    var next_page = 1;
+    if(p == undefined){
+        next_page = 1;
+    }else{
+        next_page = parseInt(p) - 1;
+    }
+    if(next_page<=0){
+        next_page=1;
+    }
+    gotoPage(next_page);
+}
+function nextpage(){
+    var currUrl = new Url(location.href);
+    var p = currUrl.query.page;
+    var next_page = 2;
+    if(p == undefined){
+        next_page = 2;
+    }else{
+        next_page = parseInt(p) + 1;
+    }
+    gotoPage(next_page);
+}
+function gotoPage(page){
+    var currUrl = new Url(location.href);
+    currUrl.query.page = page.toString();
+    window.location.href = currUrl.toString();
 }
